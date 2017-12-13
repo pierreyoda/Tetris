ba856f10259cee7ace4b6cf5ce98a95c962bf9d4
pierreyoda <pierreyoda@users.noreply.github.com>
2017-12-13 23:51:06 +0100

    Implemented the high scores board display in the game over and high scores menu Screens

--- src/view/screens/GameOverScreen.java
+++ src/view/screens/GameOverScreen.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import model.TetrisScoreManager.TetrisHighScore;
@@ -18,6 +20,7 @@ import model.TetrisScoreManager.TetrisHighScore;
 public class GameOverScreen extends Screen {
 	private static final int UPDATE_INTERVAL = 50; // we want to be responsive to a skip command
 	private static final Color BACKGROUND_COLOR = Color.BLACK;
+	private static final Color TEXT_COLOR = Color.WHITE;
 
 	/**
 	 * If true, exit the current screen.
@@ -55,9 +58,30 @@ public class GameOverScreen extends Screen {
 
 	@Override
 	public void render(final Graphics g, final Font textFont) {
-		// TODO
+		g.setColor(TEXT_COLOR);
+		g.setFont(textFont);
+
+		final int w = container().containerWidth(), h = container().containerHeight();
+
+		drawCenteredText(g, textFont, w / 2, h / 4, "----- HIGH SCORES -----");
+		for (int i = 0; i < highscores.size(); i++) {
+			final TetrisHighScore highscore = highscores.get(i);
+			if (highscore.score <= 0) break;
+
+			final int y = h / 3 + i * h / (highscores.size() * 4);
+			drawCenteredText(g, textFont,     w / 3, y, highscore.name);
+			drawCenteredText(g, textFont, 2 * w / 3, y, String.valueOf(highscore.score));
+		}
 	}
 
+	private void drawCenteredText(final Graphics g, final Font font,
+								  final int x, final int y,
+								  final String text) {
+		final FontRenderContext context = new FontRenderContext(null, true, true);
+		final Rectangle2D rect = font.getStringBounds(text, context);
+		final int posX = x - (int)(rect.getWidth() / 2), posY = y + (int)(rect.getHeight());
+		g.drawString(text, posX, posY);
+	}
 
 	@Override
 	public void keyPressed(final KeyEvent e) {
